ecd653271e1313ff220fdd4de1432e78dde0f4e6
knt9mac <matsuyamak@icloud.com>
2019-06-10 02:35:11 +0900

    [ add ] 説明追加

--- registry/comands.sh
+++ registry/comands.sh
@@ -1,3 +1,5 @@
+docker tag <イメージ名>:<tag> <レジストリのIP>:<ポート>/<任意のリポジトリ名>/<イメージ名>:<tag>
+
 docker tag lamp_service_web:latest 192.168.56.114:5000/test/lamp_service_web:1.0
 
 docker push 192.168.56.114:5000/test/lamp_service_web:1.0
